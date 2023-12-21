1ef2c59a4ace1855efb374d687121c1424e9e1a3
maurapintor <maura.pintor@gmail.com>
2023-12-21 14:40:24 +0100

    training and evaluation scripts for yolo

---
+++ predict.py
@@ -0,0 +1,112 @@
+import os
+import torch
+import torchvision.transforms as transforms
+import torch.optim as optim
+import torchvision.transforms.functional as FT
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+from model import Yolov1
+from dataset import VOCDataset
+import numpy as np
+from utils import (
+    non_max_suppression,
+    mean_average_precision,
+    intersection_over_union,
+    cellboxes_to_boxes,
+    get_bboxes,
+    plot_image,
+    load_checkpoint,
+)
+from torchvision.utils import draw_bounding_boxes
+import matplotlib.pyplot as plt
+
+seed = 8
+torch.manual_seed(seed)
+
+# Hyperparameters etc.
+DEVICE = "mps"
+BATCH_SIZE = 5
+LOAD_MODEL_FILE = "epoch_{epoch}_.pth.tar"
+IMG_DIR = "data/images"
+LABEL_DIR = "data/labels"
+
+
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, img, bboxes):
+        for t in self.transforms:
+            img, bboxes = t(img), bboxes
+
+        return img, bboxes
+
+
+transform = Compose(
+    [
+        transforms.Resize((448, 448)),
+        transforms.ToTensor(),
+    ]
+)
+
+
+
+
+def main():
+
+    model = Yolov1(split_size=7, num_boxes=2, num_classes=20).to(DEVICE)
+
+    MAX_MODEL_FILE = max(
+        filter(lambda x: x.endswith(".pth.tar"), os.listdir(".")),
+        key=lambda x: int(x.split("_")[1])
+        )
+    print("model found: ", MAX_MODEL_FILE)
+    load_checkpoint(torch.load(MAX_MODEL_FILE), model)
+
+    test_dataset = VOCDataset(
+        "data/test.csv",
+        transform=transform,
+        img_dir=IMG_DIR,
+        label_dir=LABEL_DIR,
+    )
+
+
+    test_loader = DataLoader(
+        dataset=test_dataset,
+        batch_size=BATCH_SIZE,
+        num_workers=1,
+        shuffle=True,
+        drop_last=True,
+    )
+
+    for batch_idx, (x, y) in enumerate(test_loader):
+        x, y = x.to(DEVICE), y.to(DEVICE)
+        with torch.no_grad():
+            out = model(x)
+        
+        out = cellboxes_to_boxes(out)
+        y = cellboxes_to_boxes(y)
+        
+        figure, axes = plt.subplots(figsize=(3*x.shape[0], 6), ncols=x.shape[0], nrows=2)
+        
+        for index, sample in enumerate(x):
+            image = FT.to_pil_image(sample)
+            pred = non_max_suppression(out[index], iou_threshold=0.4, threshold=0.4)
+
+            plot_image(image, y[index], axes[0, index])
+            plot_image(image, pred, axes[1, index])
+        
+        axes[0, 0].set_ylabel("ground_truth")
+        axes[1, 0].set_ylabel("prediction")
+        figure.tight_layout()
+        plt.show()
+        break
+
+
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# YOLO Implementation
+
+Credits: https://github.com/aladdinpersson/Machine-Learning-Collection/tree/master
